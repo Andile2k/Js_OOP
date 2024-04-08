a092e7a922c8507bca30c7deaff08a7c81336c2b
Andile <andile.msomi64@gmail.com>
2024-04-08 16:42:49 +0200

    Edited

--- Vehicle.js
+++ Vehicle.js
@@ -17,4 +17,4 @@ let sedan = new Vehicle('Chrysler', '300c SRT', '1.6LV8');
 console.log(sedan.name);
 console.log(sedan.maker);
 console.log(sedan.engine);
-console.log(sedan.getDetails());
+console.log(sedan.getDetails()); 
